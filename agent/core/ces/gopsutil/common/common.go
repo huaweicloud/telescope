61b3e1a80ae6912ab07aca953ef0bd70d77ae40f
hw-ces-telescope <40987747+hw-ces-telescope@users.noreply.github.com>
2018-09-10 19:32:31 +0800

    Update common.go

--- agent/core/ces/gopsutil/common/common.go
+++ agent/core/ces/gopsutil/common/common.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"os/exec"
 )
 
 func HostProc(combineWith ...string)string{
@@ -70,4 +71,111 @@ func ReadLinesOffsetN(filename string, offset uint, n int)([]string, error){
 		ret = append(ret, strings.Trim(line, "\n"))
 	}
 	return ret, nil
+}
+
+
+func GetDeviceNum(filesystem string, mountpoint string) string {
+
+	lines := getLsblkResult()
+
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) < 2 {
+			continue
+		}
+		if strings.Contains(fields[0], filesystem) && len(fields) > 6 && mountpoint == fields[6] {
+			return fields[1]
+		}
+
+	}
+	logs.GetCesLogger().Errorf("Can't get device number by mountpoint.")
+
+	return ""
+}
+
+func getLsblkResult() []string {
+	cmd := exec.Command("lsblk")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		logs.GetCesLogger().Errorf("Execute cmd StdoutPipe error, error is %s", err)
+		return nil
+	}
+	defer stdout.Close()
+	defer cmd.Wait()
+	if err = cmd.Start(); err != nil {
+		logs.GetCesLogger().Errorf("Execute cmd Start error, error is %s", err)
+		return nil
+	}
+	reader := bufio.NewReader(stdout)
+	var lines []string
+
+	for i := 0; i >= 0; i++ {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		lines = append(lines, strings.Trim(line, "\n"))
+	}
+
+	if len(lines) < 1 {
+		return nil
+	}
+
+	return lines[1:len(lines)]
+}
+
+// 根据lsblk第六列结果获取磁盘/分区类型
+func GetDeviceTypeMap() map[string][]string {
+	lines := getLsblkResult()
+	deviceTypeMap := make(map[string][]string, 5)
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) < 6 {
+			continue
+		}
+
+		switch fields[5] {
+		case "disk":
+			deviceTypeMap["disk"] = append(deviceTypeMap["disk"], fields[0])
+		case "part":
+			deviceTypeMap["part"] = append(deviceTypeMap["part"], fields[0])
+		case "lvm":
+			deviceTypeMap["lvm"] = append(deviceTypeMap["lvm"], fields[0])
+		default:
+			logs.GetCesLogger().Infof("Other value of device type from lsblk:%v", fields[0])
+		}
+
+	}
+
+	return deviceTypeMap
+}
+
+// deviceNum format (majNum:minNum)
+func GetDeviceNameByDeviceNum(deviceNum string) string {
+	deviceNumArr := strings.Split(deviceNum, ":")
+	if len(deviceNum) > 1 {
+		majNum := deviceNumArr[0]
+		minNum := deviceNumArr[1]
+
+		filename := HostProc("diskstats")
+		lines, err := ReadLines(filename)
+
+		if err != nil {
+			return ""
+		}
+		for _, line := range lines {
+			fields := strings.Fields(line)
+			if len(fields) < 3 {
+				continue
+			}
+			majNumFile := fields[0]
+			minNumFile := fields[1]
+			name := fields[2]
+
+			if majNumFile == majNum && minNumFile == minNum {
+				return name
+			}
+		}
+	}
+	return ""
 }
